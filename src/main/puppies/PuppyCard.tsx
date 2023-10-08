70831a04d3c7b76fa0e5a1e38033145b6d60654b
mysend12 <mysend12@naver.com>
2023-10-09 02:47:48 +0900

    feat: #11 쉼터 아이들 소개 영역의 인스타그램 이동 카드 디자인 적용

--- src/main/puppies/PuppyCard.tsx
+++ src/main/puppies/PuppyCard.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
 import { ReactComponent as DogManner } from '../../assets/main/puppies/feature_dog_manner.svg';
 import { ReactComponent as Health } from '../../assets/main/puppies/feature_health.svg';
 import { ReactComponent as Like } from '../../assets/main/puppies/feature_like.svg';
@@ -11,6 +12,13 @@ import { isNotBlank } from "../../common/Validation";
 import { PuppyParameter } from "./PuppiesParameters";
 import PuppyDetailModal from "./PuppyDetailModal";
 import styles from './puppies.module.scss';
+import { ReactComponent as Arrow } from '../../assets/main/puppies/go_instagram/arrow.svg';
+import { ReactComponent as Arrow1 } from '../../assets/main/puppies/go_instagram/arrow1.svg';
+
+
+import GoInstagramBackgroundImage from '../../assets/main/puppies/go_instagram/go_instagram_background.webp';
+import GoInstagramType1Image from '../../assets/main/puppies/go_instagram/go_instagram_01.webp';
+import GoInstagramType2Image from '../../assets/main/puppies/go_instagram/go_instagram_02.webp';
 
 const PuppyCard = (puppy: PuppyParameter) => {
   const [isDetail, setIsDetail] = useState(false)
@@ -53,7 +61,7 @@ const PuppyCard = (puppy: PuppyParameter) => {
     setFeatureIndexes(newArray)
     setFeatureCount(count)
   }, [])
-  
+
   return <div className={`${styles.card} ${puppy.isFamily ? styles.family : ''}`}>
     <div className={styles.puppyInfoWrapper}>
       <div className={styles.puppyName}>
@@ -126,3 +134,91 @@ const PuppyCard = (puppy: PuppyParameter) => {
 }
 
 export default PuppyCard
+
+export const GoInstagramCard = () => {
+  const [type, setType] = useState(0)
+
+  const isPc = useMediaQuery({
+    query: "(min-width:1024px)"
+  });
+
+  useEffect(() => {
+    setType(Math.floor(Math.random() * 3))
+  }, [])
+
+  return type === 0 ?
+    <GoInstagramCardType0 isPc={isPc} /> : type === 1 ?
+      <GoInstagramCardType1 isPc={isPc} /> :
+      <GoInstagramCardType2 isPc={isPc} />
+}
+
+const GoInstagramCardType0 = ({ isPc }: any) => {
+  return <Link
+    to={'https://www.instagram.com/helpshelter_pf/'}
+    target='_blank'
+    className={`${styles.card} ${styles.moveInstagram} ${styles.moveInstagramType0}`}
+    style={{ backgroundImage: `url(${GoInstagramBackgroundImage})` }}
+  >
+    <span className={styles.moveInstagramSubText}>
+      보호소에 있는 <br />
+      더 많은 아이들이 궁금하신가요?
+    </span>
+    <span className={styles.moveInstagramMainText}>
+      <span>
+        {isPc ? '지금 바로 ' : ''}인스타그램에서 확인하세요!
+      </span>
+      <Arrow width={isPc ? 14 : 8} height={isPc ? 16 : 10} />
+    </span>
+  </Link>
+}
+
+const GoInstagramCardType1 = ({ isPc }: any) => {
+  return <Link
+    to={'https://www.instagram.com/helpshelter_pf/'}
+    target='_blank'
+    className={`${styles.card} ${styles.moveInstagram} ${styles.moveInstagramType1}`}
+  >
+    <Arrow1 />
+    <img
+      src={`${GoInstagramType1Image}`}
+      alt="보호소에서 보호중인 강아지들을 모아놓은 유기견 보호소의 인스타그램 링크"
+      className={styles.moveInstagramImage1}
+    />
+    <div>
+      <span className={styles.moveInstagramSubText}>
+        보호소에 있는 <br />
+        더 많은 아이들이 궁금하신가요?
+      </span><br />
+      {isPc ? <br /> : <></>}
+      <span className={styles.moveInstagramMainText}>
+        지금 {isPc ? '바로 ' : ''}인스타그램에서 확인하세요!
+      </span>
+    </div>
+  </Link>
+}
+
+const GoInstagramCardType2 = ({ isPc }: any) => {
+  return <Link
+    to={'https://www.instagram.com/helpshelter_pf/'}
+    target='_blank'
+    className={`${styles.card} ${styles.moveInstagram} ${styles.moveInstagramType1} ${styles.moveInstagramType2}`}
+  >
+    <Arrow1 />
+    <img
+      src={`${GoInstagramType2Image}`}
+      alt="보호소에서 보호중인 강아지들을 모아놓은 유기견 보호소의 인스타그램 링크"
+      style={{marginLeft: '5.556vw', width: '9.375vw'}}
+      className={styles.moveInstagramImage1}
+    />
+    <div>
+      <span className={styles.moveInstagramSubText}>
+        보호소에 있는 <br />
+        더 많은 아이들이 궁금하신가요?
+      </span>
+      {isPc ? <div style={{height: '16px'}}></div> : <></>}
+      <span className={styles.moveInstagramMainText}>
+        지금 {isPc ? '바로 ' : ''}인스타그램에서 확인하세요!
+      </span>
+    </div>
+  </Link>
+}
