dca73727ec24680809fedca1560f402e257be43c
mysend12 <mysend12@naver.com>
2023-10-09 01:32:09 +0900

    fix: footer 영역 주석처리 (추가 개발 필요)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router';
 import './App.css';
-import Footer from './footer/Footer';
 
 function App() {
   return <>
     <Outlet />
-    <Footer />
+    {/* <Footer /> */}
   </>
 }
 
