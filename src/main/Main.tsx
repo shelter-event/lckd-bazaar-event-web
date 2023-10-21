46b619addeaab771170d6d07cf001f13df6a4f78
mysend12 <mysend12@naver.com>
2023-10-21 16:50:44 +0900

    feat: #15 JSON-LD를 이용한 SEO 최적화 추가

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -30,6 +30,20 @@ const Main = () => {
   }, [])
 
   return <div style={style}>
+    <script type="application/ld+json">
+      {JSON.stringify(bannerData)}
+    </script>
+    <script type="application/ld+json">
+      {JSON.stringify(lckdData)}
+    </script>
+    <script type="application/ld+json">
+      {JSON.stringify(puppiesData)}
+    </script>
+    <script type="application/ld+json">
+      {JSON.stringify(gameData)}
+    </script>
+
+
     <Banner />
     <LCKD />
     {isPc ? <></> : <div style={{ height: '2px', background: '#f5f5f5' }}>&nbsp;</div>}
@@ -50,3 +64,36 @@ const mainPCStyle = {
 const mainMobileStyle = {
   maxWidth: '100%',
 }
+
+// 메타데이터를 정의합니다.
+const bannerData = {
+  "@context": "http://schema.org",
+  "@type": "WebPage",
+  name: "유기견 후원 바자회 - 바자회 소개",
+  description: "유기견 후원 바자회의 상세 일정 및 장소를 소개합니다.",
+  url: "https://lckd.or.kr"
+};
+
+const lckdData = {
+  "@context": "http://schema.org",
+  "@type": "WebPage",
+  name: "유기견 보호소 LCKD - 단체 소개",
+  description: "유기견 보호소 LCKD가 하는 일들에 대해서 소개합니다.",
+  url: "https://lckd.or.kr#lckd"
+};
+
+const puppiesData = {
+  "@context": "http://schema.org",
+  "@type": "WebPage",
+  name: "유기견 보호소 LCKD - 보호 중인 유기견 소개",
+  description: "유기견 보호소 LCKD에서 보호중인 유기견들에 대해서 자세히 알아보세요.",
+  url: "https://lckd.or.kr#puppies"
+};
+
+const gameData = {
+  "@context": "http://schema.org",
+  "@type": "WebPage",
+  name: "유기견 보호소 LCKD - 유기견 짝맞추기 게임",
+  description: "유기견 보호소 LCKD에서 보호중인 유기견들과 함께 재미있는 게임을 즐겨보세요.",
+  url: "https://lckd.or.kr#game"
+};
