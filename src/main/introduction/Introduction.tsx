379a18270eb81d0395708a7bc90e0f9862a526f5
mysend12 <mysend12@naver.com>
2023-11-03 01:20:36 +0900

    feat: #18 바자회 소개 Q&A 탭 컨텐츠 적용

--- src/main/introduction/Introduction.tsx
+++ src/main/introduction/Introduction.tsx
@@ -5,8 +5,24 @@ import { useState } from 'react';
 import Dimmed from '../../common/Dimmed';
 import { Link } from 'react-router-dom';
 
+const tabTitle = ['오시는 길', '앨범', 'Q & A']
+const qnaList = [
+  [
+    'Q1. 바자회 수익은 어떻게 사용되나요?',
+    '바자회 수익은 유기동물 구조 및 치료비로 사용됩니다.'
+  ],
+  [
+    'Q2. 어떤 프로그램들이 준비되어 있나요?',
+    '1층: 바자회 물품을 구경하세요!\n2층: 댕댕이들의 영상을 관람해보세요~\n3층: 댕댕이들과 함께 놀 수 있는 옥상정원이 있어요!'
+  ],
+  [
+    'Q3. 차를 가져가고 싶은데 주차는 어디에 해야할까요?',
+    '카페 뒤쪽 주차장에 주차가 가능합니다!'
+  ],
+]
+
+
 const Introduction = () => {
-  const tabTitle = ['오시는 길', '앨범', 'Q & A']
   const [tabIndex, setTabIndex] = useState(0)
 
   return <div className={styles.introductionWrapper}>
@@ -35,24 +51,28 @@ const Introduction = () => {
           </div>
           <div className={styles.tabContent}>
             {tabIndex === 0 ?
-              <>
-                <Link
-                  to={'https://map.naver.com/p/entry/place/1147280714?c=15.30,0,0,0,dh'}
-                  target='_blank'
-                  >
-                  <div className={styles.map}>
-                    <img
-                      src={`${MapImage}`}
-                      alt="오시는길 (메종브레첼) 지도 바로가기"
-                    />
-                    <Dimmed backgroundColor={'rgba(14, 14, 14, 0.60)'} />
-                    <span className={styles.mapDetail}>지도 자세히 보기</span>
-                  </div>
-                </Link>
-              </>
+              <Link
+                to={'https://map.naver.com/p/entry/place/1147280714?c=15.30,0,0,0,dh'}
+                target='_blank'
+              >
+                <div className={styles.map}>
+                  <img
+                    src={`${MapImage}`}
+                    alt="오시는길 (메종브레첼) 지도 바로가기"
+                  />
+                  <Dimmed backgroundColor={'rgba(14, 14, 14, 0.60)'} />
+                  <span className={styles.mapDetail}>지도 자세히 보기</span>
+                </div>
+              </Link>
               : tabIndex === 1 ?
                 <></> :
-                <></>
+                <div className={styles.qna}>
+                  {qnaList.map((e) => <div className={styles.content}>
+                    <span className={styles.question}>{e[0]}</span>
+                    <span className={styles.answer}>{e[1].split('\n').map((str, index) => <>{str}<br /></>)}</span>
+                  </div>
+                  )}
+                </div>
             }
           </div>
         </div>
