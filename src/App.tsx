6a0695d82eb97b5d6c01e6378bc9e98d0746e2a1
mysend12 <mysend12@naver.com>
2023-11-02 23:51:30 +0900

    feat: #22 footer 영역 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router';
 import './App.css';
+import Footer from './footer/Footer';
 
 function App() {
   return <>
     <Outlet />
-    {/* <Footer /> */}
+    <Footer />
   </>
 }
 
--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -2,33 +2,35 @@ import { Link } from 'react-router-dom'
 import styles from './footer.module.scss'
 
 const Footer = () => {
-  return <>
-    <div style={{ height: '2px', background: '#f5f5f5' }}>&nbsp;</div>
-    <div className={styles.footer}>
-      <div>
-        <Link to={'https://pf.kakao.com/_ZxbpxkT'} target='_blank'
-          rel="noopener noreferrer">
-          입양 & 임보 & 봉사 문의
-        </Link>
-        <Link to={'https://m.blog.naver.com/PostView.naver?blogId=lcfkd&logNo=222676642630&navType=by'} target='_blank'
-          rel="noopener noreferrer">
-          LCKD 소개글
-        </Link>
-        <Link to={'https://lastchanceforkoreandogs.com/'} target='_blank'
-          rel="noopener noreferrer">
-          해외 홈페이지
-        </Link>
-        <Link to={'https://www.instagram.com/helpshelter/'} target='_blank'
-          rel="noopener noreferrer">
-          인스타그램 채널
-        </Link>
-        <Link to={'https://www.instagram.com/lastchanceforkoreandogs/'} target='_blank'
-          rel="noopener noreferrer">
-          해외 인스타그램 채널
-        </Link>
-      </div>
-    </div>
-  </>
+  // return <>
+  //   <div style={{ height: '2px', background: '#f5f5f5' }}>&nbsp;</div>
+  //   <div className={styles.footer}>
+  //     <div>
+  //       <Link to={'https://pf.kakao.com/_ZxbpxkT'} target='_blank'
+  //         rel="noopener noreferrer">
+  //         입양 & 임보 & 봉사 문의
+  //       </Link>
+  //       <Link to={'https://m.blog.naver.com/PostView.naver?blogId=lcfkd&logNo=222676642630&navType=by'} target='_blank'
+  //         rel="noopener noreferrer">
+  //         LCKD 소개글
+  //       </Link>
+  //       <Link to={'https://lastchanceforkoreandogs.com/'} target='_blank'
+  //         rel="noopener noreferrer">
+  //         해외 홈페이지
+  //       </Link>
+  //       <Link to={'https://www.instagram.com/helpshelter/'} target='_blank'
+  //         rel="noopener noreferrer">
+  //         인스타그램 채널
+  //       </Link>
+  //       <Link to={'https://www.instagram.com/lastchanceforkoreandogs/'} target='_blank'
+  //         rel="noopener noreferrer">
+  //         해외 인스타그램 채널
+  //       </Link>
+  //     </div>
+  //   </div>
+  // </>
+  return <div className={styles.footer}>
+  </div>
 }
 
 export default Footer
